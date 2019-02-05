608ccedf8f1750b45f2336d644ac8d0939cbf14d
thpe <thomas.petzoldt@tu-dresden.de>
2019-02-05 19:01:08 +0100

    release candidate: IBM chemostat demo

--- 01-chemostat/ibm-multicore.R
+++ 01-chemostat/ibm-multicore.R
@@ -1,23 +1,27 @@
 
 ## run all the scenarios on 4 CPU cores in parallel
-library(parallel)
-D <- rep(seq(0, 0.4, 0.01), each=10)
+source("chemostat-ibm-simecol.R")
+observer(ibm_test) <- function(state, time, i, out, y) {
+  S <- state$S
+  N <- nrow(state$inds)
+  c(time=time, N=N, S=S)
+}
 
+library(parallel)
 cl <- makeCluster(getOption("cl.cores", 4))
 
-ret <- parLapply(cl, 1:length(D), function(i) {
-  source("chemostat-ibm-simecol.R")
+D <- rep(seq(0, 0.4, 0.01), each=10)
+ret <- parLapply(
+  cl,
+  1:length(D),
+  function(i, D, ibm) {
+    require(simecol)
+    parms(ibm)["D"] <- D[i]
+    cbind(run=i, D=D[i], out(sim(ibm)))
+  },
+  D=D, ibm=ibm_test
+)
 
-  observer(ibm_test) <- function(state, time, i, out, y) {
-    S <- state$S
-    N <- nrow(state$inds)
-    c(time=time, N=N, S=S)
-  }
-
-  D <- rep(seq(0, 0.4, 0.01), each=10)
-  parms(ibm_test)["D"] <- D[i]
-  cbind(run=i, D=D[i], out(sim(ibm_test)))
-})
 stopCluster(cl)
 
 longtab <- rbind.fill(ret)
--- 01-chemostat/plot-ibm.R
+++ 01-chemostat/plot-ibm.R
@@ -22,19 +22,32 @@ ggplot(ret2, aes(time, S)) + geom_hex(bins=100)
 
 ibm <- ibm_test # clone the whole object
 
+observer(ibm) <- function(state, time, i, out, y) {
+  S <- state$S
+  N <- nrow(state$inds)
+  c(time=time, N=N, S=S)
+}
+
+
 ## series of _D_ilutions with 10 replicates each
-D <- rep(seq(0, 0.4, 0.05), each=2)
+D <- rep(seq(0, 0.4, 0.01), each=10)
 
 ## run all the scenarios
 ret <- lapply(1:length(D),
               FUN = function(i) {
                 cat(D[i], "\n")
                 parms(ibm)["D"] <- D[i]
-                cbind(run=i, out(sim(ibm)))
+                cbind(run=i, D=D[i], out(sim(ibm)))
                 }
               )
 
 longtab <- rbind.fill(ret)
 
-ggplot(longtab, aes(time, N)) + geom_bin2d(binwidth=c(1,10))
-ggplot(longtab, aes(time, S)) + geom_bin2d(binwidth=c(1,10))
+steady <- longtab[longtab$time == max(longtab$time), ]
+
+ggplot(steady, aes(D, N)) + geom_point()
+ggplot(steady, aes(D, S)) + geom_point()
+ggplot(steady, aes(D, D*N)) + geom_point()
+
+
+
